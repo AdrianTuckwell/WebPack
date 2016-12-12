f83c74be4e9388f211d42f9538d1dc8e0c08e3bf
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-12 13:56:25 +0000

    initial revision

---
+++ client/src/app.js
@@ -0,0 +1,25 @@
+var waterBottle = require('./water_bottle/water_bottle.js');
+
+var displayVolume = function(){
+  var volumeDisplay = document.getElementById('volume');
+  volumeDisplay.innerText = waterBottle.volume;
+}
+
+window.onload = function(){
+  console.log("app created from webpack", waterBottle.volume);
+
+  displayVolume();
+
+  var refillButton = document.getElementById('refill');
+  var drinkButton = document.getElementById('drink');
+
+  refill.onclick = function(){
+    waterBottle.fill();
+    displayVolume();
+  }
+
+  drinkButton.onclick = function(){
+    waterBottle.drink();
+    displayVolume();
+  }
+}
